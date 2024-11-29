c5c0c51d24c85d06762568a4d0dec31a03535093
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-11-29 16:05:58 -0300

    Correccion Margin Top Frame

--- src/ui/Divs/Frame.jsx
+++ src/ui/Divs/Frame.jsx
@@ -5,7 +5,7 @@ const Frame = ({ children , redirect, css}) => {
   if (!css) css = 'w-full max-w-4xl';
 
   return (
-    <div className="relative min-h-screen bg-gray-50">
+    <div className="relative mt-4 min-h-screen bg-gray-50">
       {redirect && <BackButton to={redirect} />}
       <div className="py-10">
         <div className={`mx-auto p-8 bg-white shadow-xl rounded-lg overflow-hidden ${css}`}>
