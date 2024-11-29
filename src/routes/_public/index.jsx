cea44b8603d40868d78788209cf048a344f09c99
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-11-29 16:14:26 -0300

    Se agrega Inicio

--- src/routes/_public/index.jsx
+++ src/routes/_public/index.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import Frame from '../../ui/Divs/Frame'
 
 export const Route = createFileRoute('/_public/')({
   component: RouteComponent,
@@ -7,9 +8,26 @@ export const Route = createFileRoute('/_public/')({
 
 function RouteComponent() {
   return (
-    <main className='bg-red-400'>
-      <div >Hello "/index _public/"!</div>
-
-    </main>
+    <Frame>
+      <h1 className="text-3xl font-bold text-center mb-8">Insight</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div className="col-span-1 md:col-span-2">
+          <p className="text-lg mb-4">
+            Bienvenido a <strong>Insight</strong> Proyects
+          </p>
+        </div>
+        <div className="col-span-1">
+          <img
+            src="/logo.jpg"
+            alt="Insight logo"
+            className="w-full h-auto mx-auto"
+          />
+        </div>
+      </div>
+      <hr className="my-4" />
+      <div className="text-lg text-center mb-4 italic">
+        <p></p>
+      </div>
+    </Frame>
   )
 }
