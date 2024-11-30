6718be89f6c50443976878ee10efbbc8f65b2e3c
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-11-30 17:24:29 -0300

    extraccion de FetchCurrentUser de la App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 
 import { routeTree } from './routeTree.gen'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
@@ -8,49 +8,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Spinner from './ui/loading/Spinner'
 import Error404 from './Layout/Error404'
 
-import useAuthStore from './store/useAuthStore'
-import myAxios from './api/axiosInstance'
-import config from '../config/layout'
+import useFetchCurrentUser from './hooks/useFetchCurrentUser'
 
 const queryClient = new QueryClient()
 
 function App() {
-  // Context User (precarga localstorage) - Evita F5 en publico
-  const [currentUser, setCurrentUser] = useState(() => {
-    const savedUser = localStorage.getItem('currentUser');
-    return savedUser ? JSON.parse(savedUser) : config.publicUser;
-  });
-
-  // Actualizar localStorage al cambiar el usuario actual
-  useEffect(() => {
-    localStorage.setItem('currentUser', JSON.stringify(currentUser));
-  }, [currentUser]);
-
-  const { accessToken, clearAccessToken } = useAuthStore(); // Acceso token
-
-  // Efecto para obtener el usuario actual al iniciar la app o cambiar el token
-  useEffect(() => {
-    const fetchCurrentUser = async () => {
-      if (!accessToken) {
-        console.log('No hay token, usuario no autenticado');
-        setCurrentUser(config.publicUser);
-        return;
-      }
-
-      try {
-        // console.log('Obteniendo usuario con el token:', accessToken);
-        const response = await myAxios.get('/v1/users/current');
-        console.log('Usuario autenticado:', response.data);
-        setCurrentUser(response.data);
-      } catch (error) {
-        console.error('Error al obtener el usuario:', error);
-        clearAccessToken();
-        setCurrentUser(config.publicUser);
-      }
-    };
-
-    fetchCurrentUser();
-  }, [accessToken, clearAccessToken]);
+  const { currentUser, setCurrentUser } = useFetchCurrentUser(); // hook que maneja estado Current User
 
   const router = useMemo(
     () =>
@@ -65,7 +28,7 @@ function App() {
         defaultPreloadStaleTime: 0,
         defaultNotFoundComponent: () => Error404,
       }),
-    [currentUser]
+    [currentUser, setCurrentUser]
   )
 
   if (!router) {
