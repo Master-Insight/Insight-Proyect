0635de8b6b01faec119921c7b6c1a8ded8c83048
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-12-07 16:12:47 -0300

    Mejora UI Frame

--- src/ui/Divs/Frame.jsx
+++ src/ui/Divs/Frame.jsx
@@ -3,17 +3,23 @@ import PropTypes from 'prop-types';
 import BackButton from '../buttons/BackButton2';
 
 const Frame = ({
-  children, back = false, css = 'w-full max-w-4xl',
+  children,
+  back = false,
+  css = 'w-full max-w-4xl',
+  title,
 }) => {
 
   css = back ? (css + " pt-18 p-6 mt-1") : (css + " p-6 mt-1")
 
   return (
     <div className={`relative min-h-screen bg-gray-50 ${css}`} >
-      {back && <BackButton />
-      }
+      {title ? null : back && <BackButton />}
       <div className="">
         <div className="mx-auto p-8 bg-white shadow-xl min-h-[80vh] rounded-lg overflow-hidden ">
+          {title && <div className='flex gap-3'>
+            <BackButton />
+            <h2 className="text-3xl font-semibold mb-2">{title}</h2>
+          </div>}
           {children}
         </div>
       </div>
@@ -22,9 +28,10 @@ const Frame = ({
 }
 
 Frame.propTypes = {
-  children: PropTypes.node.isRequired, // Los children deben ser elementos válidos de React
   back: PropTypes.bool, // Un booleano que habilita el botón volver atras
+  children: PropTypes.node.isRequired, // Los children deben ser elementos válidos de React
   css: PropTypes.string, // Clases CSS adicionales como string
+  title: PropTypes.string, // Título de la sección
 };
 
 export default Frame;
