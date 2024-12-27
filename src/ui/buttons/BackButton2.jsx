b9605823d9870d72b82f2f985391967c3b96a7b5
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-12-27 15:40:55 -0300

    ➕ UI BackButton2

--- src/ui/buttons/BackButton2.jsx
+++ src/ui/buttons/BackButton2.jsx
@@ -12,7 +12,7 @@ const BackButton = () => {
   return (
     <button
       onClick={handleClick}
-      className="absolute top-4 left-6 p-2 bg-gray-800 text-white rounded-full shadow-md hover:bg-gray-700 transition duration-300 flex items-center print:hidden"
+      className="p-2 bg-gray-800 text-white rounded-full shadow-md hover:bg-gray-700 transition duration-300 flex items-center print:hidden"
     >
       <Icon icon={"ic:outline-arrow-back"} className="size-6" />
       <span className="ml-2 text-sm font-semibold">Volver</span>
