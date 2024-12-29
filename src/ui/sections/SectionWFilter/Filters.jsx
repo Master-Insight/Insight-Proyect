87a4312c0fdd81d86f62a2790bcf958ba9cb9226
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-12-29 18:41:08 -0300

    📱 Mobile View Filters - se ocultan

--- src/ui/sections/SectionWFilter/Filters.jsx
+++ src/ui/sections/SectionWFilter/Filters.jsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import PropTypes from "prop-types";
 import { icons, styles, variant } from "../../../../config/layout";
+import { useState } from "react";
 
 /**
  * Componente que renderiza una sección de filtros.
@@ -16,6 +17,7 @@ import { icons, styles, variant } from "../../../../config/layout";
  */
 
 const FilterSection = ({ active = true, activeFilters, filters, onFilterChange, onReset, isPending }) => {
+  const [isVisible, setIsVisible] = useState(false); // Estado para controlar visibilidad en pantallas pequeñas
 
   if (isPending) { return <div className="text-center text-gray-500">Cargando...</div>; }
 
@@ -35,42 +37,76 @@ const FilterSection = ({ active = true, activeFilters, filters, onFilterChange, 
   };
 
   if (active) return (
-    <div className="w-full lg:w-1/5 p-4 border-r border-gray-200">
-      <div>
+    <div className="w-full md:w-2/5 lg:w-1/5 p-4 border-r border-gray-200">
+      {/* Area especial en pantallas pequeñas*/}
+      <div className="flex md:hidden items-center justify-between w-full ">
+
+        {/* Botón para mostrar/ocultar los filtros */}
+        <button
+          className="px-4 py-2 text-primary font-semibold"
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          {isVisible ? "Ocultar Filtros" : "Mostrar Filtros"}
+          {/* TODO Icono a mejorar */}
+          <Icon
+            icon={isVisible ? icons.collapse : icons.expand}
+            className="ml-2 text-lg"
+          />
+        </button>
+
+        {/* Botón para resetear los filtros */}
+        <div className="flex justify-center items-center">
+          <button
+            onClick={onReset}
+            className={`${styles.button} ${variant.primary} w-full sm:w-auto`}
+          >
+            Limpiar Filtro
+            {/* TODO Icono a mejorar */}
+            <Icon icon={icons.reset} className="ml-2 inline-block " />
+          </button>
+        </div>
+      </div>
+
+      {/* Contenedor de filtros */}
+      <div className={`${isVisible ? "block" : "hidden"} md:block mt-4 transition-all`}>
         <h3 className="text-lg font-semibold mb-4">Filtros</h3>
 
         {filters.map((filter) => (
-          <div key={filter.key} className="mb-4">
+          <div key={filter.key} className="mb-4 flex md:block">
+            <label className="mb-2 pr-4 w-5/12 sm:w-1/4 md:w-full">{filter.label}</label>
 
-            {/* Muestra la etiqueta del filtro */}
-            <label className="block mb-2">{filter.label}</label>
-
-            {/* Campo de texto */}
             {filter.type === "text" && (
               <input
                 id={filter.key}
                 type="text"
                 className="w-full p-2 border border-gray-300 rounded"
                 placeholder={`Ingresa ${filter.label.toLowerCase()}`}
-                value={activeFilters[filter.key] || ""} // Valor actual
+                value={activeFilters[filter.key] || ""}
                 onChange={(e) => onFilterChange(filter.key, e.target.value)}
               />
             )}
 
-            {/* Campo de selección */}
             {filter.type === "select" && (
               <select
                 id={filter.key}
                 className="w-full p-2 border border-gray-300 rounded"
-                multiple={filter.allowMultiple} // Habilitar selección múltiple si se especifica
+                multiple={filter.allowMultiple}
                 value={
                   filter.allowMultiple
-                    ? activeFilters[filter.key] || []  // En caso de permitir múltiples, usa array
-                    : activeFilters[filter.key] || ""  // En caso de no permitir múltiples, usa un solo valor
+                    ? activeFilters[filter.key] || []
+                    : activeFilters[filter.key] || ""
                 }
                 onChange={(e) => {
-                  const selectedOptions = Array.from(e.target.selectedOptions, (option) => option.value);
-                  onFilterChange(filter.key, filter.allowMultiple ? selectedOptions : selectedOptions[0] || "");
+                  const selectedOptions = Array.from(
+                    e.target.selectedOptions,
+                    (option) => option.value
+                  );
+                  onFilterChange(
+                    filter.key,
+                    filter.allowMultiple
+                      ? selectedOptions
+                      : selectedOptions[0] || ""
+                  );
                 }}
               >
                 {!filter.allowMultiple && (
@@ -78,19 +114,19 @@ const FilterSection = ({ active = true, activeFilters, filters, onFilterChange, 
                 )}
                 {filter.options && renderOptions(filter.options)}
               </select>
-            )
-            }
+            )}
           </div>
         ))}
       </div>
 
       {/* Botón para resetear los filtros */}
-      <div className="flex justify-center items-center">
+      <div className="hidden md:flex justify-center items-center">
         <button
           onClick={onReset}
           className={`${styles.button} ${variant.primary} w-full sm:w-auto`}
         >
           Limpiar Filtro
+          {/* TODO Icono a mejorar */}
           <Icon icon={icons.reset} className="ml-2 inline-block " />
         </button>
       </div>
